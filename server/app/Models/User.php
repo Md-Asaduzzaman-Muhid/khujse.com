70febd67a244eac855816a4d41b8c3c14f46adb3
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-02-29 20:45:07 +0600

    update changes

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $guarded = ['id'];
 
     /**
      * The attributes that are mass assignable.
